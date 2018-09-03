3e6ee4d02543a7ffc77a624e5c548145cb4edf50
Dennis Duda <git@seri.tools>
2018-09-03 15:00:51 +0200

    Add discord webhook script

--- status.sh
+++ status.sh
@@ -4,5 +4,6 @@ diff /root/orig_full.asm compare_full.asm > /tmp/asm.diff || true
 insertions=$(cat /tmp/asm.diff | grep "^+" | wc -l)
 deletions=$(cat /tmp/asm.diff | grep "^-" | wc -l)
 total=$(cat /root/orig_full.asm | wc -l)
-echo "$(((insertions + deletions) / (total * 2 / 100)))% change, $((insertions)) insertions(+), $((deletions)) deletions(-)"
-
+accuracy="$(((insertions + deletions) / (total * 2 / 100)))% change, $((insertions)) insertions(+), $((deletions)) deletions(-)"
+echo "$accuracy" > accuracy.txt
+echo "$accuracy"
