1eb4bdb5a125f56e1638107571798376e2569715
Anders Jenbo <anders@jenbo.dk>
2019-08-30 08:45:52 +0200

    Add hellfire and spawn status scripts

---
+++ hellfire-status.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+diff /root/hellfire-orig_full.asm compare_full.asm > /tmp/asm.diff || true
+insertions=$(cat /tmp/asm.diff | grep -v "asm" | grep "^+" | grep -vc ';')
+deletions=$(cat /tmp/asm.diff | grep -v "asm" | grep "^-" | grep -vc ';')
+total=$(grep -v ^$ /root/orig_full.asm | grep -vc ';')
+accuracy="$(((insertions + deletions) / (total * 2 / 100)))% change, $((insertions)) insertions(+), $((deletions)) deletions(-)"
+echo "$accuracy" > accuracy.txt
+echo "$accuracy"
---
+++ spawn-status.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+diff /root/orig_full.asm spawn-compare_full.asm > /tmp/asm.diff || true
+insertions=$(cat /tmp/asm.diff | grep -v "asm" | grep "^+" | grep -vc ';')
+deletions=$(cat /tmp/asm.diff | grep -v "asm" | grep "^-" | grep -vc ';')
+total=$(grep -v ^$ /root/orig_full.asm | grep -vc ';')
+accuracy="$(((insertions + deletions) / (total * 2 / 100)))% change, $((insertions)) insertions(+), $((deletions)) deletions(-)"
+echo "$accuracy" > accuracy.txt
+echo "$accuracy"
